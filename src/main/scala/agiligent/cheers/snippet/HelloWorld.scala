c6f5eca5f5f815d4fa6c2368b6e922f6b4f4ca2b
Gyo Hamamoto <gyo@hamamoto.org>
2011-02-07 23:26:12 +0900

    Added hello world snippet.

---
+++ src/main/scala/agiligent/cheers/snippet/HelloWorld.scala
@@ -0,0 +1,19 @@
+package agiligent.cheers.snippet
+
+import xml.NodeSeq
+import net.liftweb.util.Helpers._
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gyo
+ * Date: 2/7/11
+ * Time: 10:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class HelloWorld {
+  def howdy(xhtml: NodeSeq): NodeSeq =
+    bind("b", xhtml,
+      "time" -> (new _root_.java.util.Date).toString,
+      "hello" -> "Hello World!")
+}
